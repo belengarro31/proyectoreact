064eb5bb5167281df20595290d2c1c0d2b77edc8
Belen Garro <belengarro31@gmail.com>
2023-05-20 00:39:50 -0300

    formok

--- src/componentes/CartContainer/CartContainer.jsx
+++ src/componentes/CartContainer/CartContainer.jsx
@@ -1,9 +1,41 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContex";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { useState } from "react";
+
+
 
 export const CartConteiner = () =>{
+    const [dataForm, setDataForm]=useState({
+        name:"",
+        phone:"",
+        email:""
+    }) 
     const{cartList, vaciarCarrito, totalCarrito, borrarCantidad, cantidadTotal} = useCartContext()
 
+    const generarOrden=(evt)=>{
+        evt.preventDefault()
+        const order = {}
+        order.buyer=dataForm
+        order.items=cartList.map(({name, id, price})=>({id, name, price}))
+        order.total=totalCarrito()
+
+        const dbFirestore=getFirestore()
+        const ordersCollection=collection(dbFirestore, "orders")
+
+        addDoc(ordersCollection, order)
+        .then(resp=>console.log(resp))
+    }
+
+    const handleOnChange = (evt)=>{
+        console.log('nombre del input',evt.target.name)
+        console.log('valor del input',evt.target.value)
+        setDataForm({
+            ...dataForm,
+            [evt.target.name]: evt.target.value
+        })
+        } 
+    
     if (cantidadTotal() === 0 ) {
         return(
             <div className='text-center p-5 m-5'>
@@ -26,10 +58,33 @@ export const CartConteiner = () =>{
                 <div className="cartTotal">
                         <h3>Total a pagar: $ { totalCarrito() } </h3>
                 </div>
-            <button onClick={vaciarCarrito} className="btn btn-outline-danger">Vaciar carrito</button>              
-        </div>
-      
-    )    
-    
-}
+            <button onClick={vaciarCarrito} className="btn btn-outline-danger">Vaciar carrito</button>  
 
+             <form classname="formFinal" onSubmit={generarOrden}>
+            <input 
+                    type='text' 
+                    name="name" 
+                    onChange={handleOnChange}
+                    value={dataForm.name} 
+                    placeholder="ingrese el nombre" 
+                /> 
+
+                <input 
+                    type='text' 
+                    name="phone" 
+                    onChange={handleOnChange}
+                    value={dataForm.phone} 
+                    placeholder="ingrese el tel " 
+                /> 
+                <input 
+                    type='text' 
+                    name="email" 
+                    onChange={handleOnChange}
+                    value={dataForm.email} 
+                    placeholder="ingrese el email" 
+                /> 
+ 
+                <button className="btn btn-outline-danger">Generar orden</button> 
+                </form>
+            </div>    
+    )}
